3b8fb05447254b75505c9d1de614cda45929feff
Iasonas, Psomadakis <iasonas.ps@gmail.com>
2024-11-17 15:39:46 +0200

    initial windows

---
+++ PacketSniffer(Csharp)/Menu.cs
@@ -0,0 +1,48 @@
+﻿using SharpPcap;
+
+internal static class Menu
+{
+    internal static void MainMenu()
+    {
+        string input = "";
+        while (input != "0")
+        {
+            Console.Clear();
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) Begin packet sniffing");
+            Console.WriteLine("2) Print CaptureDeviceList");
+            Console.WriteLine("9) Exit");
+            Console.Write("\r\nSelect an option: \n");
+            input = Console.ReadLine() ?? "";
+            string pattern = Utilities.GeneratePatternLine();
+
+
+            switch (input)
+            {
+                case "1":
+                    Console.WriteLine(pattern);
+                    Utilities.StartPacketSniffing();
+                    Console.WriteLine(pattern);
+                    break;
+                case "2":
+                    var devices = CaptureDeviceList.Instance;
+                    Console.WriteLine(pattern);
+                    devices.ToList().ForEach(li =>
+                    {
+                        Console.WriteLine(li);
+                        Console.WriteLine(pattern);
+                    });
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                    break;
+                case "9":
+                    Console.WriteLine("Exiting the application...");
+                    //ExitApplication();
+                    return;
+                default:
+                    Console.WriteLine("Invalid option. Please select a valid option.");
+                    break;
+            }
+        }
+    }
+}
--- PacketSniffer(Csharp)/Program.cs
+++ PacketSniffer(Csharp)/Program.cs
@@ -1,60 +1,7 @@
-﻿using PacketDotNet;
-using SharpPcap.LibPcap;
-using SharpPcap;
-using System;
-using System.Net;
-using System.Net.Sockets;
-using System.Runtime.InteropServices;
-
-class Program : Utilities
+﻿class Program : Utilities
 {
-
-
     public static async Task Main()
     {
-        MainMenu();
-    }
-
-    private static void MainMenu()
-    {
-        string input = "";
-        while (input != "0")
-        {
-            Console.Clear();
-            Console.WriteLine("Choose an option:");
-            Console.WriteLine("1) Begin packet sniffing");
-            Console.WriteLine("2) Print CaptureDeviceList");
-            Console.WriteLine("9) Exit");
-            Console.Write("\r\nSelect an option: \n");
-            input = Console.ReadLine() ?? "";
-            string pattern = Utilities.GeneratePatternLine();
-
-
-            switch (input)
-            {
-                case "1":
-                    Console.WriteLine(pattern);
-                    Utilities.StartPacketSniffing();
-                    Console.WriteLine(pattern);
-                    break;
-                case "2":
-                    var devices = CaptureDeviceList.Instance;
-                    Console.WriteLine(pattern);
-                    devices.ToList().ForEach(li =>
-                    {
-                        Console.WriteLine(li);
-                        Console.WriteLine(pattern);
-                    });
-                    Console.ReadLine();
-                    break;
-                case "9":
-                    Console.WriteLine("Exiting the application...");
-                    //ExitApplication();
-                    return;
-                default:
-                    Console.WriteLine("Invalid option. Please select a valid option.");
-                    break;
-            }
-        }
+        Menu.MainMenu();
     }
 }
